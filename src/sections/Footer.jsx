53683baf7d4f410a702b23b863858fcc02f375ba
MemoSy <ahmeddalhalabi1@gmail.com>
2023-08-21 21:54:42 +0300

    first commit

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,13 @@
+import { copyrightSign } from "../assets/icons";
+import { footerLogo } from "../assets/images";
+import { footerLinks, socialMedia } from "../constants";
+
+const Footer = () => {
+  return (
+    <footer className='max-container'>
+      <h1>footer</h1>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/sections/Subscribe.jsx
@@ -0,0 +1,11 @@
+import { Button } from "../components";
+
+const Subscribe = () => {
+  return (
+    <section>
+      Subsicribe
+    </section>
+  );
+};
+
+export default Subscribe;
